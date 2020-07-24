5310ef4689e4ae94aa6a983a05965a3f0d0ff736
Jailson Pereira <jailson.pereira@itriad.org.br>
2020-07-24 09:12:39 -0400

    add: feature read file

--- process-data/elastic.py
+++ process-data/elastic.py
@@ -1,18 +1,44 @@
 from elasticsearch import Elasticsearch
 import logging
+import time
 
-user = "elastic"
-passwd = "YgcFDLqEuhvxafts9Ulv8KHJ"
-es = Elasticsearch(["https://elastic:YgcFDLqEuhvxafts9Ulv8KHJ@c01160965fdb47918c3d06933a6df089.us-east-1.aws.found.io:9243"])
 
 index = "vendas-analysis"
 
-def insert_data(data):
-    res=es.index(index=index, body=data,)
-    
-    if res == False:
-        logging.info("[ELASTICSEARCH][INSERT][ERROR]")
+
+class ElasticService:
+    def __init__(self):
+        self.client = Elasticsearch("https://elastic:YgcFDLqEuhvxafts9Ulv8KHJ@c01160965fdb47918c3d06933a6df089.us-east-1.aws.found.io:9243")
+        self.index = index
+        self.doc_type = '_doc'
+
+    def on_check_index(self):
+        '''
+        Ckeck index exists
+        '''
+        if not self.client.indices.exists(index=self.index):
+            logging.info("[ELASTICSEARCH][INDEX][NO-EXIST]")
+            self.client.indices.create(index=self.index, ignore=400)
+            time.sleep(2)
+
+            logging.info("[ELASTICSEARCH][INDEX][CREATE][SUCESSUFULLY]")
+        else:
+            logging.info("[ELASTICSEARCH][INDEX][EXISTS]")
+
+   
+    def on_insert_data(self, data):
+        '''
+        Insert document in elasticsearch
+        '''
+        self.on_check_index()
         
-    logging.info("[ELASTICSEARCH][INSERT][SUCESSUFULLY]")
+        res =  self.client.index(index=self.index, doc_type='_doc', body=data,)
+        if res == False:
+            logging.info("[ELASTICSEARCH][INSERT][ERROR]")
+            return False
+
+        logging.info("[ELASTICSEARCH][INSERT][SUCESSUFULLY]")
+        return True
+
 
         
